23de39bb15f303530d918a7d1b237516b305ca01
kickuu <prprpr0521@i.softbank.jp>
2020-09-25 13:27:11 +0900

    first commit

---
+++ Assets/Scripts/TweetControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CoreTweet;
+
+public class TweetControler : MonoBehaviour
+{
+    // MainScript mainScript;
+    [SerializeField] public InputField tweetTextInputField;
+
+    void Start()
+    {
+    }
+
+    public void Tweet()
+    {
+        // InputField tweetText = tweetTextInputField.GetComponent<InputField>();
+        // MainScript mainScript = GameObject.Find("AppManager").GetComponent<MainScript>();
+        // MainScript mainScript = GetComponent<MainScript>();
+        string tweetText = tweetTextInputField.text;
+        if (tweetText != "")
+        {
+            MainScript.token.Statuses.Update(status => tweetText);
+            Debug.Log("Successful Tweet!");
+        }
+        else
+        {
+            Debug.Log("Error! Non text!!");
+        }
+    }
+}
